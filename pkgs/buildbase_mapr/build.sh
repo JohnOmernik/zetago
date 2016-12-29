319b35b8fc933e70e1be466037ac7f32fd08d90e
johnomernik <john@omernik.com>
2016-12-29 15:04:53 -0600

    Added Build base

---
+++ pkgs/buildbase_mapr/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+checkdocker
+check4dockerimage "${APP_IMG_NAME}" BUILD
+reqdockerimg "zetabase_mapr"
+
+if [ "$BUILD" == "Y" ]; then
+    rm -rf $BUILD_TMP
+    mkdir -p $BUILD_TMP
+    cd $BUILD_TMP
+
+    # Since BUILD is now "Y" The vers file actually makes the dockerfile
+    . ${MYDIR}/${APP_PKG_BASE}/${APP_VERS_FILE}
+
+    sudo docker build -t $APP_IMG .
+    sudo docker push $APP_IMG
+
+    cd $MYDIR
+    rm -rf $BUILD_TMP
+    echo ""
+    @go.log INFO "$APP_NAME package build with $APP_VERS_FILE"
+    echo ""
+else
+    @go.log WARN "Not rebuilding $APP_NAME"
+fi
+
+
