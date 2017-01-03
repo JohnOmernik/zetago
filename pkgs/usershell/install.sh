341f621cb02a900aec6a2819c1fe089d85056016
johnomernik <john@omernik.com>
2017-01-03 15:44:13 -0600

    Updated usershell installs

---
+++ pkgs/usershell/install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+###############
+# $APP Specific
+echo "Resources"
+read -e -p "Please enter the amount of Memory per Usershell: " -i "1024" APP_MEM
+read -e -p "Please enter the amount of CPU shares to limit Usershell: " -i "1.0" APP_CPU
+echo ""
+
+
+mkdir -p ${APP_HOME}/marathon
+
+
+cp ${APP_PKG_BASE}/lib/profile_template ${APP_HOME}/
+cp ${APP_PKG_BASE}/lib/nanorc_template ${APP_HOME}/
+cp ${APP_PKG_BASE}/lib/bashrc_template ${APP_HOME}/
+
+
+cat > $APP_HOME/instance_include.sh << EOL1
+#!/bin/bash
+APP_MEM="$APP_MEM"
+APP_CPU="$APP_CPU"
+APP_IMG="$APP_IMG"
+EOL1
