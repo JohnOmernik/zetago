0e4effe93b6ff4725c56e880433fb69fcf072380
johnomernik <john@omernik.com>
2017-01-04 10:26:52 -0600

    Update to usershell to allow for spark

--- pkgs/usershell/start.sh
+++ pkgs/usershell/start.sh
@@ -136,7 +136,7 @@ EOS
         fi
     fi
     if [ -d "$SPARK_PKG_HOME" ]; then
-        SPARK_HOME_SHORT=$(ls -1 ${SPARK_PKG_HOME}|grep -v "run\.sh"|grep -v "${APP_ID}\.conf")
+        SPARK_HOME_SHORT=$(ls -1 ${SPARK_PKG_HOME}|grep -v "run\.sh"|grep -v "${PKG_ID}\.conf")
         SPARK_HOME="${SPARK_PKG_HOME}/$SPARK_HOME_SHORT"
 
         echo "Using $SPARK_HOME for spark home"
