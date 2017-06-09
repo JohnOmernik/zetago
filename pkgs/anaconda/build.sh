47cd7fbc0fe263194409794976ec7fe52c713437
johnomernik <john@omernik.com>
2017-06-09 06:29:39 -0500

    Updates

--- pkgs/anaconda/build.sh
+++ pkgs/anaconda/build.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
 checkdocker
+check4dockerimage "${APP_IMG_NAME}" BUILD
+reqdockerimg "zetabase"
 
-# Build both version of Anaconda
-for (( i = 1; i < 2; i++ )); do
+if [ "$BUILD" == "Y" ]; then
+    rm -rf $BUILD_TMP
+    mkdir -p $BUILD_TMP
+    cd $BUILD_TMP
 
-    APP_IMG_NAME=$(echo -e "APP_IMG_NAME${i}" |  tr -d '[:space:]')
-    APP_IMG=$(echo -e "APP_IMG${i}" |  tr -d '[:space:]')
-    APP_IMG=$(echo -e "APP_IMG_TAG${i}" |  tr -d '[:space:]')
+    # Since BUILD is now "Y" The vers file actually makes the dockerfile
+    . ${MYDIR}/${APP_PKG_BASE}/${APP_VERS_FILE}
 
-    check4dockerimage "${!APP_IMG_NAME}" BUILD
+    sudo docker build -t $APP_IMG .
+    sudo docker push $APP_IMG
 
-    if [ "$BUILD" == "Y" ]; then
-        rm -rf ${BUILD_TMP}
-        mkdir -p ${BUILD_TMP}
-        cd ${BUILD_TMP}
+    cd $MYDIR
+    rm -rf $BUILD_TMP
+    echo ""
+    @go.log INFO "$APP_NAME package build with $APP_VERS_FILE"
+    echo ""
+else
+    @go.log WARN "Not rebuilding $APP_NAME"
+fi
 
-        bash ${MYDIR}/${APP_PKG_BASE}/${APP_VERS_FILE} ${i}
-
-        sudo docker build --rm -t ${!APP_IMG} .
-        sudo docker push ${!APP_IMG}
-
-        cd ${MYDIR}
-        rm -rf ${BUILD_TMP}
-        echo ""
-        @go.log INFO "${APP_NAME} package build with ${APP_VERS_FILE}"
-        echo ""
-    else
-        @go.log WARN "Not rebuilding ${APP_NAME}"
-    fi
-
-done
 
