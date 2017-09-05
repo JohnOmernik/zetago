ad54bd23a8e7a95173920e81f2c781f1ee1de627
johnomernik <john@omernik.com>
2017-09-05 06:08:00 -0500

    Added updates to anaconda base package

--- pkgs/anaconda/build.sh
+++ pkgs/anaconda/build.sh
@@ -5,7 +5,13 @@ check4dockerimage "${APP_IMG_NAME}" BUILD
 reqdockerimg "${REQ_APP_IMG_NAME}"
 
 
+
 if [ "$BUILD" == "Y" ]; then
+
+    echo "In addition to anaconda2 and anaconda3 base images, we can also add additional libraries for running Apache Spark in Anaconda. Do you wish to do that?"
+    read -e -p "Build additional images anaspark2 and anaspark3 based on anaconda images? (Y/N): " -i "Y" BUILD_SPARK
+    echo ""
+
 
     rm -rf $BUILD_TMP
     mkdir -p $BUILD_TMP
